26dace7ec671b4155eb1b6d037dfe2d2c7ba5a01
syrine65 <syrine.chennoufi@polytechnique.edu>
2024-05-22 07:25:38 +0000

    create kernel and wrapper

---
+++ mul-sigmoid.cu
@@ -0,0 +1,16 @@
+#include <math.h>
+
+__device__ float sigmoid(float x) {
+    return 1.0f / (1.0f + expf(-x));
+}
+
+__global__ void elementwise_multiply_sigmoid(float* A, float* C, int width, int height) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int idx = y * width + x;
+
+    if (x < width && y < height) {
+        float val = A[idx];
+        C[idx] = val * sigmoid(val);
+    }
+}
